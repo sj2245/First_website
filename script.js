8412e1f5a26b39896c57eb785b3d63187d75dfe3
sj2245 <shakespearejamesii@gmail.com>
2023-07-02 15:15:56 -0400

    Very cool transition/effect that makes youtube cry

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log(`Connected:Initiate Rizzing of the females via Javascript`)
+function getdoc(selector) {
+  return document.querySelector(selector);
+}
+let OGbutt = getdoc(`.theOGButton`);
+console.log(OGbutt);
+
+OGbutt.addEventListener(`click`, function (e){
+  OGbutt.classList.toggle(`circleButton`);
+})
