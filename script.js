9607e364b4d78716bf415e419dc381aae8fa14c2
sj2245 <shakespearejamesii@gmail.com>
2023-07-05 20:09:46 -0400

    Very epic click counter(not a catastrophe)

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 console.log(`Connected: Initiate Rizzing of the females via Javascript`);
+
 function getdoc(selector) {
   return document.querySelector(selector);
 }
+
+let count = 0;
 let OGbutt = getdoc(`.theOGButton`);
-console.log(OGbutt);
+let buttswap = `gamer`;
+let buttString = `Guide to rizzing up females the ${buttswap} way, ${count}`;
 
 OGbutt.addEventListener(`click`, function (e) {
+  count = count + 1;
   OGbutt.classList.toggle(`circleButton`);
+  
+  if (buttswap != `programmer`) {
+    buttswap = `programmer`;
+  } 
+  else {
+    buttswap = `gamer`;
+  }
+  
+  buttString = `Guide to rizzing up females the ${buttswap} way, ${count}`;
+  OGbutt.innerHTML = buttString;
 })
